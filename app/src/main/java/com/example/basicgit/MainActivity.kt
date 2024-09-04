3309292b54dbf66d1a73ff90e80e412a7b65b41d
weaam_alagha <renukaweeam@hotmail.com>
2024-09-04 18:55:21 +0800

    push after rebase

--- app/src/main/java/com/example/basicgit/MainActivity.kt
+++ app/src/main/java/com/example/basicgit/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
 
-                        name = "This is the third test",
+                        name = "This is the forth test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
